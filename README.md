91d11f2dba0be7a3189c456c89a8fd311d20b6b1
furinara <furinara@qq.com>
2025-09-30 01:31:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# verilens README
+
+verilens will provide SystemVerilog syntax highlighting and snippets support.
+
+## Features
+
+This is a lightweight extension providing SystemVerilog syntax highlighting support.
+
+**Enjoy!**
---
+++ src/extension.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+exports.deactivate = deactivate;
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+function activate(context) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    console.log('Congratulations, your extension "verilens" is now active!');
+}
+// This method is called when your extension is deactivated
+function deactivate() { }
+//# sourceMappingURL=extension.js.map
